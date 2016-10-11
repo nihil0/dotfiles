c429a5d545e7adb9359daa8b535f83bc7ab1acf3
Neelabh Kashyap <neelabh2@gmail.com>
2016-10-12 01:43:43 +0300

    removed powerline install and added NERDTree

--- init.sh
+++ init.sh
@@ -11,9 +11,8 @@ curl -so ~/.vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vi
 # Setup solarized colors
 git clone git://github.com/altercation/vim-colors-solarized.git
 mv vim-colors-solarized ~/.vim/bundle/
-# Install powerline
-git clone https://github.com/powerline/powerline.git
-mv powerline ~/.vim/bundle/
+# Install NERDTree
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 # Install jedi-vim
 git clone --recursive https://github.com/davidhalter/jedi-vim.git
 mv jedi-vim ~/.vim/bundle/
