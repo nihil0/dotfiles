e655880ae3e65a77b5cb82e3b0c3ff213ffd4929
Neelabh Kashyap <neelabh2@gmail.com>
2016-10-11 15:08:30 +0300

    removed inelegant cd commands.. attempt 2

--- vim-init.sh
+++ vim-init.sh
@@ -4,13 +4,13 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -so ~/.vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
 # Setup solarized colors
-git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle
-
+git clone git://github.com/altercation/vim-colors-solarized.git
+mv vim-colors-solarized ~/.vim/bundle/
 #Install powerline
-git clone https://github.com/powerline/powerline.git ~/.vim/bundle
-
+git clone https://github.com/powerline/powerline.git
+mv powerline ~/.vim/bundle/
 #Install jedi-vim
-git clone --recursive https://github.com/davidhalter/jedi-vim.git ~/.vim/bundle
-
+git clone --recursive https://github.com/davidhalter/jedi-vim.git
+mv jedi-vim ~/.vim/bundle/
 #Clean up
 rm -rf dotfiles
